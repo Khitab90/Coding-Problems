e6ac10058360dae2fce4de8c9b84b66a8b0a0eff
Khitab90 <khitabanand435@gmail.com>
2023-04-07 18:12:55 -0700

    update recurring-decimal

--- HashMap/recurring-decimal.js
+++ HashMap/recurring-decimal.js
@@ -5,33 +5,42 @@ function fractionToDecimal(numerator, denominator) {
     let result = "",
         remainderMap = {};
 
+    // If numberator is 0, retunr 0 in the string
     if (numerator == 0) {
         return "0";
     }
 
-    if ((numerator < 0) ^ (denominator < 0)) {
+    // If numerator or denominator is negative, append "-" to the result
+    if ((numerator < 0) || (denominator < 0)) {
         result += "-";
-
+        // Make numerator and denominator +ve after adding "-" to the result
         numerator = Math.abs(numerator);
         denominator = Math.abs(denominator);
     }
 
+    // Calculate the quotient and remainder
     let quotient = numerator / denominator;
     let remainder = (numerator % denominator) * 10;
+    // Append the quotient part in the result
     result += toInteger(quotient).toString();
 
     if (remainder == 0) return result;
     else {
+        // Append "." before the right part
         result += ".";
 
+        // Right side of the decimal point
         while (remainder != 0) {
+            // If digits are repeating, it means the reminader is already present in the hashmap
             if (Object.keys(remainderMap).includes(`${remainder}`)) {
+                // Convert fraction to recurring decimal
                 let beginning = remainderMap[remainder];
                 let left = result.substring(0, beginning);
                 let right = result.substring(beginning, result.length);
                 result = left + "(" + right + ")";
                 return result;
             }
+            // Otherwise put the remainder in the hashmap
             remainderMap[remainder] = result.length;
             quotient = remainder / denominator;
             result += toInteger(quotient).toString();
