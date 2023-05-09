2a86489f2122e9798c27d788dc676cbac00789ac
Khitab90 <khitabanand435@gmail.com>
2023-05-08 17:14:31 -0700

    stacks - nested iterator statement

--- Stacks/nested-iterator.js
+++ Stacks/nested-iterator.js
@@ -1,3 +1,9 @@
+// You’re given a nested list of integers. Each element is either an integer or a list whose elements may also be integers or other integer lists. 
+// Your task is to implement an iterator to flatten the nested list. You will have to implement the Nested Iterator class. This class has the following functions:
+//      Constructor: This initializes the iterator with the nested list.
+//      Next (): This returns the next integer in the nested list.
+//      Has Next (): This returns TRUE if there are still some integers in the nested list. Otherwise, it returns FALSE.
+
 import NestedIntegers from "./nested_integers.js";
 
 // This function inializes the stack using the
